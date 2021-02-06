4d9dca542c34b09d6cd2f07af1c296a4124bdc47
Shambhavi <shambhavis20@gmail.com>
2021-02-07 00:07:23 +0530

    changed the button hover effect

--- components/blogpage/NavMenu.js
+++ components/blogpage/NavMenu.js
@@ -4,26 +4,12 @@ import styles from '../../styles/blogstyle.module.css'
      render(){
         return(
             <div className={styles.navigationmenu}>
-                 <div className={styles.search}>
-                <input type="text" className={styles.searchTerm} placeholder="What are you looking for?"></input>
-                <button type="submit" className={styles.searchButton}>
-                <i class="fa fa-search"></i>
-                </button>
-                </div>
+                 
                 <div className={styles.blogh5}>ARCHIVE</div>
                 
                 <div className={styles.thinseparatorline}></div>
-                    <div className={styles.date}>January 2020</div>
-                    <div className={styles.date}>January 2020</div>
-                    <div className={styles.date}>January 2020</div>
-                    <div className={styles.date}>January 2020</div>
-                    <div className={styles.date}>January 2020</div>
-                    <div className={styles.date}>January 2020</div>
-                    <div className={styles.date}>January 2020</div>
-                    <div className={styles.date}>January 2020</div>
-                    <div className={styles.date}>January 2020</div>
-                    <div className={styles.date}>January 2020</div>
-                    <div className={styles.date}>January 2020</div>
+                    <div className={styles.date}>April 2018</div>
+                    
                                                 
             </div>
          )
