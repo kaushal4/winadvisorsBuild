1d184b7de971e0a3f395367c813bd82cc11b63df
Shambhavi <shambhavis20@gmail.com>
2021-02-15 17:46:53 +0530

    navigator added symbols

--- components/FixedNavigator.js
+++ components/FixedNavigator.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "../styles/navigator.module.css";
 export default class FixedNavigator extends React.Component{
     constructor(){
         super();
@@ -7,8 +8,17 @@ export default class FixedNavigator extends React.Component{
     render(){
         return(
             <div className="fixedElement">
-                contact <br />
-                us
+                <img className={styles.symbol} src="/symbols/phone.png"></img>
+                <a href="tel:+ 91 9150471122" className={styles.tooltiptext}>+ 91 9150471122</a>
+                <img className={styles.symbol} src="/symbols/pin.png"></img>
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                <img className={styles.symbol} src="/symbols/whatsapp.png"></img>
+                <img className={styles.symbol} src="/symbols/facebbook.png"></img>
+                <img className={styles.symbol} src="/symbols/linkedin.png"></img>
+                <img className={styles.symbol} src="/symbols/twitter.png"></img>
+                <div className={styles.followus}>US</div>
+                <br></br><br></br>
+                <div className={styles.followus}>FOLLOW</div>
             </div>
         )
     }
