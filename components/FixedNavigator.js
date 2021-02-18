3cde029c7a1ea3102740fa8c6f88a536729305b0
Shambhavi <shambhavis20@gmail.com>
2021-02-18 19:11:59 +0530

    closes #3

--- components/FixedNavigator.js
+++ components/FixedNavigator.js
@@ -8,17 +8,26 @@ export default class FixedNavigator extends React.Component{
     render(){
         return(
             <div className="fixedElement">
-                <img className={styles.symbol} src="/symbols/phone.png"></img>
-                <a href="tel:+ 91 9150471122" className={styles.tooltiptext}>+ 91 9150471122</a>
-                <img className={styles.symbol} src="/symbols/pin.png"></img>
-                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-                <img className={styles.symbol} src="/symbols/whatsapp.png"></img>
-                <img className={styles.symbol} src="/symbols/facebbook.png"></img>
-                <img className={styles.symbol} src="/symbols/linkedin.png"></img>
-                <img className={styles.symbol} src="/symbols/twitter.png"></img>
+                <div className={styles.up}>
+                
+                <a href="+91 8735629720" className={styles.tooltiptext}>
+                <img className={styles.symbol} src="/symbols/navigator/phone.png"></img>
+                </a>
+                <a href="407,4th Floor,Sanjar Enclave,Opposite-PVR Cinema, S.V.Road, Kandivali(West), Mumbai 400 067" className={styles.tooltiptext}>
+                <img className={styles.symbol} src="/symbols/navigator/pin.png"></img>
+                </a>
+                </div>
+                <div className={styles.down}>
+                <a href="https://wa.me/+919082114257"><img className={styles.symbol} src="/symbols/navigator/whatsapp.png"></img></a>
+                <a href="https://www.facebook.com/WIN-CORPORATE-ADVISORS-193426394758642"><img className={styles.symbol} src="/symbols/navigator/facebook.png"></img></a>
+                <a href="https://www.linkedin.com/company/wincorporateadvisors/"><img className={styles.symbol} src="/symbols/navigator/linkedin.png"></img></a>
+                <a href="https://twitter.com/AdvisorsWin"><img className={styles.symbol} src="/symbols/navigator/twitter.png"></img></a>
+                <div className={styles.followusdiv}>
                 <div className={styles.followus}>US</div>
                 <br></br><br></br>
                 <div className={styles.followus}>FOLLOW</div>
+                </div>
+                </div>
             </div>
         )
     }
