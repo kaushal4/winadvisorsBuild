d085e8fbed3e1a1003c12d3cf84ff8dc8a85c690
Kaushal Damania <damaniakaushal4@gmail.com>
2021-05-14 23:06:46 +0530

    fixed react import bug in contact us page

--- components/contactusPage/MainPage.js
+++ components/contactusPage/MainPage.js
@@ -1,48 +1,53 @@
-import React from 'React';
-import styles from '../../styles/contactus.module.css'
-class MainPage extends React.Component{
-    render(){
-        return (
-            <div className={styles.mainpage}>
-                <div className={styles.contactTiles}>
-                <div className={styles.contactTile}>
-                    <img src="./images/email.png"></img>
-                    <p>Expect us to respond within 48 hours.</p>
-                </div>
-                <div className={styles.contactTile}>
-                <img src="./images/fax.jpg"></img>
-                    <p>Contact us through fax</p>
-                </div>
-                <div className={styles.contactTile}>
-                <img src="./images/call.png"></img>
-                    <p>We are waiting to hear from you.</p>
-                </div>
-                </div>
-                
-                <div className={styles.contactform}>  
-                <form className={styles.contact} action="" method="post">
-                    <h3>Contact Us</h3>
-                    <h4>Feel free to reach us here and we’ll get back to you as soon as possible</h4>
-                    <label for="name">NAME:</label>
-                    <input  type="text" tabindex="1" required autofocus></input>
-                    <label for="email">EMAIL:</label>
-                    <input  type="email" tabindex="2" required></input>
-                    <label for="contact">CONTACT NO:</label>
-                    <input  type="tel" tabindex="3" required></input>
-                    <label for="message">MESSAGE:</label>
-                    <textarea  name="message" required ></textarea>
-                    
-                    <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">SUBMIT</button>
-                   
-                </form>
-                
-                
-                </div>
-                
-            </div>
-            
-        )
-    }
+import React from "react";
+import styles from "../../styles/contactus.module.css";
+class MainPage extends React.Component {
+  render() {
+    return (
+      <div className={styles.mainpage}>
+        <div className={styles.contactTiles}>
+          <div className={styles.contactTile}>
+            <img src="./images/email.png"></img>
+            <p>Expect us to respond within 48 hours.</p>
+          </div>
+          <div className={styles.contactTile}>
+            <img src="./images/fax.jpg"></img>
+            <p>Contact us through fax</p>
+          </div>
+          <div className={styles.contactTile}>
+            <img src="./images/call.png"></img>
+            <p>We are waiting to hear from you.</p>
+          </div>
+        </div>
+
+        <div className={styles.contactform}>
+          <form className={styles.contact} action="" method="post">
+            <h3>Contact Us</h3>
+            <h4>
+              Feel free to reach us here and we’ll get back to you as soon as
+              possible
+            </h4>
+            <label for="name">NAME:</label>
+            <input type="text" tabindex="1" required autofocus></input>
+            <label for="email">EMAIL:</label>
+            <input type="email" tabindex="2" required></input>
+            <label for="contact">CONTACT NO:</label>
+            <input type="tel" tabindex="3" required></input>
+            <label for="message">MESSAGE:</label>
+            <textarea name="message" required></textarea>
+
+            <button
+              name="submit"
+              type="submit"
+              id="contact-submit"
+              data-submit="...Sending"
+            >
+              SUBMIT
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MainPage;
